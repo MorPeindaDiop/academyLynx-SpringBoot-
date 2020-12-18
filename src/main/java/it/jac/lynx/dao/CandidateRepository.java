764155c344712ad84243e62d8dc2ab895f464a7a
LocaTheProgrammer <pietro.locatelli00@gmail.com>
2020-12-18 12:23:50 +0100

    creazione test

--- src/main/java/it/jac/lynx/dao/CandidateRepository.java
+++ src/main/java/it/jac/lynx/dao/CandidateRepository.java
@@ -2,6 +2,7 @@ package it.jac.lynx.dao;
 
 import java.util.List;
 
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,7 @@ import it.jac.lynx.entity.Candidate;
 public interface CandidateRepository extends CrudRepository<Candidate, Integer> {
 
 	public List<Candidate> findByidSeniority(int idSeniority);
+
+	public List<Candidate> findByIdTest(int id);
 
 }
